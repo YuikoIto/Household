9b18252d3672deb5c28fb29de80ec8b07fcb192b
YuikoIto <toffywing@gmail.com>
2020-07-22 23:38:25 +0900

    make style better

--- lib/main.dart
+++ lib/main.dart
@@ -37,10 +37,11 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter app'),
+        title: Text('家計簿'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        //mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             width: double.infinity,
