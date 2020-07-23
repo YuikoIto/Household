ed65dceb75ca95051ceecab1d0857004dd6ca6fc
YuikoIto <toffywing@gmail.com>
2020-07-23 23:18:16 +0900

    add input value

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,8 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now(),
     ),
   ];
+  String titleInput;
+  String amountInput;
 
   @override
   Widget build(BuildContext context) {
@@ -62,9 +64,15 @@ class MyHomePage extends StatelessWidget {
                 children: <Widget>[
                   TextField(
                     decoration: InputDecoration(labelText: 'Title'),
+                    onChanged: (val) {
+                      titleInput = val;
+                    },
                   ),
                   TextField(
                     decoration: InputDecoration(labelText: 'Amount'),
+                    onChanged: (val) {
+                      amountInput = val;
+                    },
                   ),
                   FlatButton(
                     child: Text('Add'),
